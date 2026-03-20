03a3a447ff190f8f44793662bb0625cdb5f04034
Alexey Savelev <yy9863715@gmail.com>
2026-03-20 12:24:01 +0300

    move authmanager, etc. to separate files

--- server/include/database.hpp
+++ server/include/database.hpp
@@ -1,10 +1,7 @@
-//
-// Created by asavelev on 2/17/26.
-//
-
 #ifndef SERVER_DATABASE_HPP
 #define SERVER_DATABASE_HPP
 
+#include <mutex>
 #include <pqxx/pqxx>
 #include <string>
 
@@ -40,89 +37,19 @@ public:
 
     [[nodiscard]] bool isConnected() const { return conn.is_open(); }
 
-protected:
-    const std::string conn_string;
-    mutable pqxx::connection conn;
-
-    void execute(std::string command) const {
+    template<typename ...Args>
+    auto execute(const std::string& command, Args&&... args) {
+        std::unique_lock l(mutex);
         pqxx::work txn(conn);
-        txn.exec(std::move(command));
+        auto res = txn.exec_params(command, std::forward<Args>(args)...);
         txn.commit();
-    }
-};
-
-// Responsible for user-related stuff.
-// Includes but is not limited to hours, admin status, ban status
-struct UserManager : Database {
-    explicit UserManager(std::string connection_string)
-        : Database(std::move(connection_string)) {
-    };
-
-    explicit UserManager(const Database &db)
-        : UserManager(db.get_conn_string()) {
+        return res;
     }
 
-    void addUserHours(const std::string &id, int amount = 2);
-
-    void banUser(const std::string &id, int duration);
-
-    bool isAdmin(const std::string &id) const;
-
-    [[nodiscard]] bool userExists(const std::string &id) const;
-
-    [[nodiscard]] int getUserHours(const std::string &id) const;
-
-    [[nodiscard]] int getUnbanTime(const std::string &id) const;
-
-    void deleteUser(const std::string &id);
-};
-
-// Essentially everything associated with auth
-// Includes but is not limited to emails, passwords, JWT(?), IDs
-struct AuthManager : Database {
-    explicit AuthManager(std::string conn_string)
-        : Database(std::move(conn_string)) {
-    }
-
-    explicit AuthManager(const Database &db)
-        : AuthManager(db.get_conn_string()) {
-    }
-
-    [[nodiscard]] std::string getUserIdEmail(const std::string &email) const;
-
-    [[nodiscard]] std::string getUserIdByEmail(const std::string &email) const;
-
-    [[nodiscard]] std::string getPasswordByEmail(const std::string &email) const;
-
-    [[nodiscard]] bool emailExists(const std::string &email) const;
-
-    void createUser(const std::string &email, const std::string &password,
-                    const std::string &name);
-};
-
-// I suppose you can guess what it does by the name. Come on. We're not
-// 5-year-olds here. Go on.
-struct SlotManager : Database {
-    explicit SlotManager(std::string conn_string)
-        : Database(std::move(conn_string)) {
-    }
-
-    explicit SlotManager(const Database &db)
-        : SlotManager(db.get_conn_string()) {
-    }
-
-    void changeSlotInfo(const std::string &id, const std::string &start_time,
-                        const std::string &end_time, int capacity);
-
-    void closeSlot(const std::string &id);
-
-    void removeEntry(const std::string &user_id, const std::string &slot_id);
-
-    void addEntry(const std::string &user_id, const std::string &slot_id);
-
-    pqxx::result getSlotInfo(const std::string &id) const;
-
-    std::vector<std::string> getGymList() const; // TODO
+private:
+    const std::string conn_string;
+    mutable std::mutex mutex;
+    mutable pqxx::connection conn;
 };
 
 #endif // SERVER_DATABASE_HPP

410edcbdf2e02ccdd8a802e1229e31f4607084f8
Alexey Savelev <yy9863715@gmail.com>
2026-03-20 12:24:40 +0300

    use normal functions not lambdas

--- server/include/routes/auth_routes.hpp
+++ server/include/routes/auth_routes.hpp
@@ -1,7 +1,3 @@
-//
-// Created by asavelev on 3/9/26.
-//
-
 #ifndef TRUTEN_SERVER_AUTH_ROUTES_HPP
 #define TRUTEN_SERVER_AUTH_ROUTES_HPP
 
@@ -9,55 +5,64 @@
 #include "database.hpp"
 #include "request_handler.hpp"
 #include "response_builder.hpp"
+#include "auth_manager.hpp"
 
 struct AuthRoutes {
 public:
     using crow::json::type::String;
 
-    explicit AuthRoutes(const Database &db) : db_auth(db) {
+    explicit AuthRoutes(Database &db) : db_auth(db) {
     }
 
-    // I think the password should be hashed by the time this function is called
+    crow::response createAccount(const crow::request &req) {
+        RequestHandler request(req);
+        request.require("email", String);
+        request.require("password", String);
+        request.require("name", String);
+        if (!request.responseIsOk()) {
+            return ResponseBuilder(request).build();
+        }
+        const auto email = static_cast<std::string>(request["email"]);
+        const auto password = static_cast<std::string>(request["password"]);
+        const auto name = static_cast<std::string>(request["name"]);
+        db_auth.createUser(email, password, name);
+        const std::string user_id = db_auth.getUserIdByEmail(email);
+        return ResponseBuilder(RESPONSE_CODE::OK).addField("userId", user_id).build();
+    }
+
+    crow::response login(const crow::request &req) const {
+        RequestHandler request(req);
+        request.require("email", String);
+        request.require("password", String);
+        if (!request.responseIsOk()) {
+            return ResponseBuilder(request).build();
+        }
+        const auto email = static_cast<std::string>(request["email"]);
+        const auto password = static_cast<std::string>(request["password"]);
+        const std::string real_password = db_auth.getPasswordByEmail(email);
+        const std::string user_id = db_auth.getUserIdByEmail(email);
+        if (password == real_password) {
+            // TODO: not mvp, but add JWT
+            return ResponseBuilder(RESPONSE_CODE::OK).addField("userId", user_id).build();
+        }
+        return ResponseBuilder(RESPONSE_CODE::NO_ACCESS).build();
+    }
+
     void registerRoutes(crow::SimpleApp &app) {
         CROW_ROUTE(app, "/v1/auth/createAccount")
                 .methods("POST"_method)([this](const crow::request &req) {
-                    RequestHandler request(req);
-                    request.require("email", String);
-                    request.require("password", String);
-                    request.require("name", String);
-                    if (!request.responseIsOk()) {
-                        return ResponseBuilder(request).build();
-                    }
-                    const auto email = static_cast<std::string>(request["email"]);
-                    const auto password = static_cast<std::string>(request["password"]);
-                    const auto name = static_cast<std::string>(request["name"]);
-                    db_auth.createUser(email, password, name);
-                    const std::string user_id = db_auth.getUserIdByEmail(email);
-                    return ResponseBuilder(RESPONSE_CODE::OK).addField("userId", user_id).build();
+                    return createAccount(req);
                 });
 
         CROW_ROUTE(app, "/v1/auth/login")
                 .methods("POST"_method)([this](const crow::request &req) {
-                    RequestHandler request(req);
-                    request.require("email", String);
-                    request.require("password", String);
-                    if (!request.responseIsOk()) {
-                        return ResponseBuilder(request).build();
-                    }
-                    const auto email = static_cast<std::string>(request["email"]);
-                    const auto password = static_cast<std::string>(request["password"]);
-                    const std::string real_password = db_auth.getPasswordByEmail(email);
-                    const std::string user_id = db_auth.getUserIdByEmail(email);
-                    if (password == real_password) {
-                        // TODO: not mvp, but add JWT
-                        return ResponseBuilder(RESPONSE_CODE::OK).addField("userId", user_id).build();
-                    }
-                    return ResponseBuilder(RESPONSE_CODE::NO_ACCESS).build();
+                    return login(req);
                 });
         // TODO: not mvp, but add JWT
         CROW_ROUTE(app, "/v1/auth/refresh")
                 .methods("POST"_method)(
-                    [](const crow::request &req) { return ResponseBuilder().build(); });
+                    [](const crow::request &req) { return ResponseBuilder().build(); }
+                    );
     }
 
 private:

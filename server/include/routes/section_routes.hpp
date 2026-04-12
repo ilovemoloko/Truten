146f90ad33152bcfbb7eccbb164777555a47e905
Alexey Savelev <yy9863715@gmail.com>
2026-04-13 00:08:06 +0300

    gym list is now yaml-accurate

--- server/include/routes/section_routes.hpp
+++ server/include/routes/section_routes.hpp
@@ -24,6 +24,7 @@ public:
         crow::json::wvalue slot_list = std::move(slots);
         crow::json::wvalue final;
         final["slots"] = std::move(slot_list);
+
         return {std::move(final)};
     }
 
